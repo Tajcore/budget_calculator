b2ce7026a986897482e1886af2a9b5b3b77b9350
Tahjyei Thompson <45900878+Tajcore@users.noreply.github.com>
2020-11-18 10:55:49 -0500

    Small Fix #4

--- app.js
+++ app.js
@@ -21,18 +21,18 @@ app.use('/api', require('./routes/api/rest'));
 
 // create static assets from react code for production only
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static( 'client/build' ));
+    app.use(express.static( 'client2/build' ));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client2', 'build', 'index.html'));
     });
 }
 
 if (process.env.NODE_ENV === 'testing') {
-    app.use(express.static( 'client/build' ));
+    app.use(express.static( 'client2/build' ));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client2', 'build', 'index.html'));
     });
 }
 
